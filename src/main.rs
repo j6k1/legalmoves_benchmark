8862b8945851cfce7861ffd64a08aa5fb7e27574
jinpu <jinpu@DESKTOP-3N3HFJE>
2021-01-16 20:42:21 +0900

    is_nyugyoku_winの計測を追加

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate usiagent;
 
 use std::time::{Instant};
-use std::collections::HashMap;
 
 use usiagent::rule::*;
 use usiagent::protocol::*;
-use usiagent::event::*;
 use usiagent::shogi::*;
 
 const DEPTH:u32 = 4;
@@ -14,25 +12,12 @@ const INITIAL_SFEN:&'static str = "sfen l6nl/5+P1gk/2np1S3/p1p4Pp/3P2Sp1/1PPb2P1
 fn main() {
 	let position_parser = PositionParser::new();
 
-	let (teban, banmen, mc, _, _) = match position_parser.parse(&INITIAL_SFEN.split(" ").collect::<Vec<&str>>()).unwrap() {
-		position => match position {
-			SystemEvent::Position(teban, p, n, m) => {
-				let(banmen,mc) = match p {
-					UsiInitialPosition::Startpos => {
-						(BANMEN_START_POS.clone(), MochigomaCollections::Pair(HashMap::new(),HashMap::new()))
-					},
-					UsiInitialPosition::Sfen(ref b,MochigomaCollections::Pair(ref ms,ref mg)) => {
-						(b.clone(),MochigomaCollections::Pair(ms.clone(),mg.clone()))
-					},
-					UsiInitialPosition::Sfen(ref b,MochigomaCollections::Empty) => {
-						(b.clone(),MochigomaCollections::Pair(HashMap::new(),HashMap::new()))
-					}
-				};
-				(teban,banmen,mc,n,m)
-			},
-			_ => {
-				panic!("invalid state.");
-			}
+	let (teban, banmen, mc, _, _) = match  position_parser.parse(&INITIAL_SFEN.split(" ").collect::<Vec<&str>>()) {
+		Ok(position) => {
+			position.extract()
+		},
+		_ => {
+			panic!("invalid state.");
 		}
 	};
 
@@ -83,6 +68,26 @@ fn main() {
 	count += c;
 
 	let elapsed = t.duration_since(start_time);
+
+	let count_scaled = count as u128 * 1000_000_000;
+	let elapsed_scaled = elapsed.as_secs() as u128 * 1000_000_000 + elapsed.subsec_nanos() as u128;
+
+	println!("{}.{:?}秒経過しました。", elapsed.as_secs(), elapsed.subsec_nanos() / 1_000_000);
+	println!("{}個の指し手を生成しました。", count);
+	println!("1秒あたり{}個の指し手を生成しました。", count_scaled / elapsed_scaled);
+
+	println!("is_nyugyoku_win...");
+
+	let start_time = Instant::now();
+
+	let mut count = 0;
+
+	let _ = Rule::is_nyugyoku_win(&state, teban, &mc, &None);
+	let c = process_moves_with_is_nyugyoku_win(teban,&state,&mc,&mvs,DEPTH-1,count,start_time);
+
+	count += c;
+
+	let elapsed = start_time.elapsed();
 
 	let count_scaled = count as u128 * 1000_000_000;
 	let elapsed_scaled = elapsed.as_secs() as u128 * 1000_000_000 + elapsed.subsec_nanos() as u128;
@@ -170,4 +175,28 @@ fn process_moves_with_oute_only_moves(teban:Teban,
 	}
 
 	(count,time)
+}
+
+fn process_moves_with_is_nyugyoku_win(teban:Teban,
+								   state:&State, mc:&MochigomaCollections,
+								   mvs:&Vec<LegalMove>, depth:u32, count:usize, mut time:Instant) -> usize {
+	if depth == 0 {
+		return mvs.len();
+	}
+
+	let mut count = 0;
+
+	for m in mvs {
+		let next = Rule::apply_move_none_check(state,teban,mc,m.to_applied_move());
+
+		match next {
+			(ref next,ref mc,_) => {
+				let _ = Rule::is_nyugyoku_win(next,teban,mc,&None);
+				let mvs:Vec<LegalMove> = Rule::legal_moves_all(teban, next, mc);
+				count += process_moves(teban.opposite(),next,mc,&mvs,depth-1);
+			}
+		}
+	}
+
+	count
 }
